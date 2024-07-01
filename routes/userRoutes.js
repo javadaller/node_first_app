d35cb3027649ee4fc62294297f0f17a076a16fa8
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-07-01 16:44:20 +0200

    fini

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,3 @@
-const express = require("express");
 const jsonfile = require("jsonfile");
 const path = require("path");
 
@@ -17,6 +16,10 @@ module.exports = (app) => {
 
     app.post("/users/new", (req, res) => {
         const { email, username } = req.body;
+
+        if (!email || !username) {
+            return res.status(400).send("Email and username are required");
+        }
 
         jsonfile.readFile(filePath, (err, content) => {
             if (err) {
@@ -39,6 +42,10 @@ module.exports = (app) => {
     app.delete("/users/destroy", (req, res) => {
         const { email } = req.body;
 
+        if (!email) {
+            return res.status(400).send("Email is required");
+        }
+
         jsonfile.readFile(filePath, (err, content) => {
             if (err) {
                 console.error("Error reading file:", err);
@@ -48,7 +55,7 @@ module.exports = (app) => {
             const userIndex = content.findIndex(user => user.email === email);
 
             if (userIndex !== -1) {
-                const removedUser = content.splice(userIndex, 1);
+                content.splice(userIndex, 1);
 
                 jsonfile.writeFile(filePath, content, { spaces: 2 }, (err) => {
                     if (err) {
@@ -57,6 +64,61 @@ module.exports = (app) => {
                     }
                     res.status(200).send("User removed successfully");
                 });
+            } else {
+                res.status(404).send("User not found");
+            }
+        });
+    });
+
+    app.put("/user", (req, res) => {
+        const { username } = req.body;
+        const { email } = req.query;
+
+        if (!email) {
+            return res.status(400).send("Email is required");
+        }
+        if (!username) {
+            return res.status(400).send("Username is required");
+        }
+
+        jsonfile.readFile(filePath, (err, content) => {
+            if (err) {
+                console.error("Error reading file:", err);
+                return res.status(500).send("Error reading file");
+            }
+
+            const user = content.find(user => user.email === email);
+
+            if (user) {
+                user.username = username;
+
+                jsonfile.writeFile(filePath, content, { spaces: 2 }, (err) => {
+                    if (err) {
+                        console.error("Error writing file:", err);
+                        return res.status(500).send("Error writing file");
+                    }
+                    res.status(200).send(`User ${email} updated successfully`);
+                });
+            } else {
+                res.status(404).send("User not found");
+            }
+        });
+    });
+
+    app.get("/user", (req, res) => {
+        const { email } = req.query;
+
+        jsonfile.readFile(filePath, (err, content) => {
+            if (err) {
+                console.error("Error reading file:", err);
+                return res.status(500).send("Error reading file");
+            }
+
+            const user = content.find(user => user.email === email);
+
+            if (user) {
+                console.log("Found user", user);
+                res.json(user);
             } else {
                 res.status(404).send("User not found");
             }
